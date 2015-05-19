dc21e32ceb05f7f661c892257cb7e2d2a57b3df3
didik nurdyansah <didiknurdyansah@gmail.com>
2015-05-19 15:12:12 +0700

    modified srstheme.info

--- template.php
+++ template.php
@@ -5,15 +5,15 @@
  * template.php
  */
  
- function uttheme_theme() {
+ function srstheme_theme() {
   $items = array();
   // create custom user-login.tpl.php
   $items['user_login'] = array(
   'render element' => 'form',
-  'path' => drupal_get_path('theme', 'uttheme') . '/templates',
+  'path' => drupal_get_path('theme', 'srstheme') . '/templates',
   'template' => 'user-login',
   'preprocess functions' => array(
-  'uttheme_preprocess_user_login'
+  'srstheme_preprocess_user_login'
   ),
  );
 return $items;
